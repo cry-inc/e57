08192b35a5a7b80c458f8b7c4e4b739d65b18604
cry-inc <cry-inc@users.noreply.github.com>
2023-02-27 21:33:59 +0100

    Extended unpacking implementation for colors, intensity and invalid states to work for more files

--- src/bitpack.rs
+++ src/bitpack.rs
@@ -49,7 +49,7 @@ impl BitPack {
                     // These values can require 9 bytes before alignment
                     // which would not fit into the u64 used for decoding!
                     Error::not_implemented(format!(
-                        "Integers with {bit_size} bits are not supported"
+                        "Scaled integers with {bit_size} bits are not supported"
                     ))?
                 }
                 let mut mask = 0_u64;
@@ -84,10 +84,47 @@ impl BitPack {
             RecordType::Integer { min, max } => {
                 let range = max - min;
                 let bit_size = f64::ceil(f64::log2(range as f64 + 1.0)) as u64;
-                if bit_size % 8 != 0 {
-                    Error::not_implemented("Integers are only supported for multiples of 8 bit")?
+                if bit_size > 56 && bit_size != 64 {
+                    // These values can require 9 bytes before alignment
+                    // which would not fit into the u64 used for decoding!
+                    Error::not_implemented(format!(
+                        "Integers with {bit_size} bits are not supported"
+                    ))?
                 }
-                let byte_size = f64::ceil((bit_size as f64) / 8.0) as usize;
+                let mut mask = 0_u64;
+                for i in 0..bit_size {
+                    mask |= 1 << i;
+                }
+                let mut result = Vec::with_capacity((stream.available() / bit_size) as usize);
+                loop {
+                    if stream.available() < bit_size {
+                        break;
+                    }
+                    let e = stream.extract(bit_size).internal_err(
+                        "Unexpected error when extracing integer from byte stream",
+                    )?;
+                    let mut tmp = [0_u8; 8];
+                    tmp[..e.data.len()].copy_from_slice(&e.data);
+                    let uint_value = (u64::from_le_bytes(tmp) >> e.offset) & mask;
+                    let float_value = uint_value as f32 / range as f32;
+                    result.push(float_value);
+                }
+                Ok(result)
+            }
+            RecordType::ScaledInteger { min, max, .. } => {
+                let range = max - min;
+                let bit_size = f64::ceil(f64::log2(range as f64 + 1.0)) as u64;
+                if bit_size > 56 && bit_size != 64 {
+                    // These values can require 9 bytes before alignment
+                    // which would not fit into the u64 used for decoding!
+                    Error::not_implemented(format!(
+                        "Scaled integers with {bit_size} bits are not supported"
+                    ))?
+                }
+                let mut mask = 0_u64;
+                for i in 0..bit_size {
+                    mask |= 1 << i;
+                }
                 let mut result = Vec::with_capacity((stream.available() / bit_size) as usize);
                 loop {
                     if stream.available() < bit_size {
@@ -96,15 +133,10 @@ impl BitPack {
                     let e = stream.extract(bit_size).internal_err(
                         "Unexpected error when extracing scaled integer from byte stream",
                     )?;
-                    if e.bits != bit_size || e.offset != 0 {
-                        Error::internal(
-                            "Unexpected data after extracting scaled integer from byte stream",
-                        )?
-                    }
                     let mut tmp = [0_u8; 8];
-                    tmp[..byte_size].copy_from_slice(&e.data);
-                    let int_value = u64::from_le_bytes(tmp) as i64;
-                    let float_value = int_value as f32 / range as f32;
+                    tmp[..e.data.len()].copy_from_slice(&e.data);
+                    let uint_value = (u64::from_le_bytes(tmp) >> e.offset) & mask;
+                    let float_value = uint_value as f32 / range as f32;
                     result.push(float_value);
                 }
                 Ok(result)
@@ -120,26 +152,27 @@ impl BitPack {
             RecordType::Integer { min, max } => {
                 let range = max - min;
                 let bit_size = f64::ceil(f64::log2(range as f64 + 1.0)) as u64;
-                if bit_size != 1 {
-                    Error::not_implemented(
-                        "Unpacking to u8 is currently only possible for binary values",
+                if bit_size > 8 {
+                    Error::internal(
+                        "Unpacking integers with {bit_size} bits to u8 does not work",
                     )?
+                }
+                let mut mask = 0_u64;
+                for i in 0..bit_size {
+                    mask |= 1 << i;
                 }
                 let mut result = Vec::with_capacity((stream.available() / bit_size) as usize);
                 loop {
                     if stream.available() < bit_size {
                         break;
                     }
-                    let e = stream
-                        .extract(bit_size)
-                        .internal_err("Unexpected error when extracing integer from byte stream")?;
-                    if e.bits != bit_size {
-                        Error::internal(
-                            "Unexpected data after extracting integer from byte stream",
-                        )?
-                    }
-                    let mask = 1 << e.offset;
-                    result.push(if e.data[0] & mask == 0 { 0 } else { 1 });
+                    let e = stream.extract(bit_size).internal_err(
+                        "Unexpected error when extracing integer from byte stream",
+                    )?;
+                    let mut tmp = [0_u8; 8];
+                    tmp[..e.data.len()].copy_from_slice(&e.data);
+                    let uint_value = (u64::from_le_bytes(tmp) >> e.offset) & mask;
+                    result.push(uint_value as u8);
                 }
                 Ok(result)
             }
