866bf531f3de1c362141b90b346d16213023b1a4
cry-inc <cry-inc@users.noreply.github.com>
2024-08-19 10:46:02 +0200

    Update C++ test data generator to latest version of libe57format library

--- testdata/cpp_generator/build_and_run.ps1
+++ testdata/cpp_generator/build_and_run.ps1
@@ -1,7 +1,10 @@
 if (Test-Path build) { Remove-Item -Recurse -Force build }
 if (Test-Path vcpkg) { Remove-Item -Recurse -Force vcpkg }
 git clone https://github.com/Microsoft/vcpkg.git
-./vcpkg/bootstrap-vcpkg.bat
+cd vcpkg
+git apply ../libe57format.diff
+cd ..
+./vcpkg/bootstrap-vcpkg.bat -disableMetrics
 mkdir build | out-null
 cmake -B ./build -S . -DCMAKE_TOOLCHAIN_FILE="./vcpkg/scripts/buildsystems/vcpkg.cmake"
 cmake --build ./build --config Debug
