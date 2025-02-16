d2fa88943f69d68164158880ed76dfa014443648
cry-inc <cry-inc@users.noreply.github.com>
2025-02-16 15:26:18 +0100

    Updated script to generate C++ library examples (removed custom patch for latest version since VCPKG port has been updated)

--- testdata/cpp_generator/build_and_run.ps1
+++ testdata/cpp_generator/build_and_run.ps1
@@ -1,9 +1,6 @@
 if (Test-Path build) { Remove-Item -Recurse -Force build }
 if (Test-Path vcpkg) { Remove-Item -Recurse -Force vcpkg }
 git clone https://github.com/Microsoft/vcpkg.git
-cd vcpkg
-git apply ../libe57format.diff
-cd ..
 ./vcpkg/bootstrap-vcpkg.bat -disableMetrics
 mkdir build | out-null
 cmake -B ./build -S . -DCMAKE_TOOLCHAIN_FILE="./vcpkg/scripts/buildsystems/vcpkg.cmake"
